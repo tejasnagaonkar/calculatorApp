ffa018c54fa0b90725f1950e0813fb0490251859
tejasnagaonkar <tejas.nagaonkar@gmail.com>
2024-11-28 18:56:44 +0530

    Initial commit of simple React calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Calculator = () => {
+    const [input, setInput] = useState('');
 
-export default App;
+    const handleClick = (value) => {
+        setInput(input + value);
+    };
+
+    const handleClear = () => {
+        setInput('');
+    };
+
+    const handleEqual = () => {
+        try {
+            setInput(eval(input));
+        } catch {
+            setInput('Error');
+        }
+    };
+
+    return (
+        <div className="calculator">
+            <input type="text" value={input} readOnly />
+            <div>
+                <button onClick={() => handleClick('1')}>1</button>
+                <button onClick={() => handleClick('2')}>2</button>
+                <button onClick={() => handleClick('3')}>3</button>
+                <button onClick={() => handleClick('+')}>+</button>
+            </div>
+            <div>
+                <button onClick={() => handleClick('4')}>4</button>
+                <button onClick={() => handleClick('5')}>5</button>
+                <button onClick={() => handleClick('6')}>6</button>
+                <button onClick={() => handleClick('-')}>-</button>
+            </div>
+            <div>
+                <button onClick={() => handleClick('7')}>7</button>
+                <button onClick={() => handleClick('8')}>8</button>
+                <button onClick={() => handleClick('9')}>9</button>
+                <button onClick={() => handleClick('*')}>*</button>
+            </div>
+            <div>
+                <button onClick={() => handleClear()}>C</button>
+                <button onClick={() => handleClick('0')}>0</button>
+                <button onClick={() => handleEqual()}>=</button>
+                <button onClick={() => handleClick('/')}>/</button>
+            </div>
+        </div>
+    );
+};
+
+export default Calculator;
